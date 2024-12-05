ac9767c46ea54946942042908bd013b7a741be5a
McCune1224 <alexmccune1224@gmail.com>
2024-12-05 18:19:57 -0500

    project init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<section class="bg-primary-content">
+	<h1>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+	<h2>Okay now this is epic</h2>
+</section>
